98e9abc120b4061a2ffb2104900129f4b681c68b
Sammy Pineda <sappcg@gmail.com>
2022-04-07 21:01:41 -0500

    fin seccion 3

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Counter title="Entrada"/>
+  <Counter :start="12" title="Salida"/>
+  <Counter/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from '@/components/Counter.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Counter
   }
 }
 </script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,53 @@
+<template>
+    <h2>{{costomTitle}} </h2>
+    <p>{{count}} <sup>2</sup> = {{squareValue}} </p>
+
+    <button @click="decrement()">-</button>
+    <button @click="increment()">+</button>
+</template>
+
+<script>
+export default {
+    props:{
+        title: {
+            type: String,
+            default: 'Counter'
+        },
+        start: {
+            type: Number,
+            default: 1,
+            validator: function(value) {
+                return value > 100;
+            }
+        }
+    },
+    data() {
+        return {
+            count: this.start
+        }
+    },
+    methods: {
+        increment() {
+            this.count++
+        },
+        decrement() {
+            this.count--
+        },
+        getSquareValue() {
+            return this.count * this.count
+        }
+    },
+    computed: {
+        squareValue() {
+            return this.count * this.count
+        },
+        costomTitle() {
+            return this.title ? this.title : 'Counter';
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
